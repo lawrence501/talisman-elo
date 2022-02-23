b4651fd82c94304f4d90c80fd91a794d8aa54594
lawrence501 <22851435+lawrence501@users.noreply.github.com>
2022-02-24 00:30:49 +1100

    Changing character data format to support auto-sort

--- main.py
+++ main.py
@@ -53,8 +53,11 @@ def updateElo(char1, char2, char1Score=1):
         characterData[char1] = newRating1
         characterData[char2] = newRating2
 
+        sortedData = sorted(list(characterData.items()),
+                       key=lambda k: characterData[k[0]])
+
         f.seek(0)
-        json.dump(characterData, f, indent=2)
+        json.dump(sortedData, f, indent=2)
         f.truncate()
 
         verb = "defeated"
